ed5423a21f4614eb655ed96249ec9ea79c3eae53
Gizmo83 <p_hoang83@yahoo.com>
2018-04-19 17:00:09 -0700

    Updated API.js and Bootstrap CDN on index.html

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -19,9 +19,7 @@ export default {
   },
   // Search articles from NYT Api
   searchArticles: function(topic, startYear, endYear) {
-
-    let queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=b9f91d369ff59547cd47b931d8cbc56b:0:74623931&q=" + topic + "&begin_date=" + startYear + "0101" + "&end_date=" + endYear + "0101";
-    
+    let queryUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=b9f91d369ff59547cd47b931d8cbc56b:0:74623931&q=${topic}&begin_date=${startYear}0101&end_date=${endYear}0101`;
     return axios.get(queryUrl);
   }
 };
